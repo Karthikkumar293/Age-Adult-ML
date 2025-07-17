54a97d1e70e0624a32a1968e82a22e6cd5d6b7b8
Karthikkumar293 <kumarkarthik5665@gmail.com>
2025-07-17 07:01:06 +0530

    Update karthik.LogisticRegression.py

--- karthik.LogisticRegression.py
+++ karthik.LogisticRegression.py
@@ -11,7 +11,7 @@ df2 = pd.DataFrame({
 })
 
 # Print without index
-print(df2.head(10).to_string(index=False))# taking the data set from ai
+print(df2.head(10).to_string(index=False))# --taking  data set from ai--
 
 df2.head(1000)
 df2.isnull()
